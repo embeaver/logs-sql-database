2241662af1235413e355ea591947a179fc49372e
Erika B <50300032+embeaver@users.noreply.github.com>
2019-06-08 20:48:25 -0400

    Count popular authors
    Add popular authors function, clean up python SQL in top articles function, clean up print methods

--- report.py
+++ report.py
@@ -1,10 +1,11 @@
 import psycopg2
 import bleach
 import datetime
+### USING PYTHON3 ###
 
+DBNAME = "news"
 
-DBNAME = 'news'
-
+# maybe not needed
 def delete_spam():
     """Deletes spam from database"""
     db = psycopg2.connect(database = DBNAME)
@@ -20,64 +21,61 @@ def delete_spam():
     # close the database
     db.close()
 
-#delete_spam()
-
-#def combine_authors():
-#    """Return all posts from the database"""
-#    db = psycopg2.connect(database=DBNAME)
-#    c = db.cursor()
-#    # join article authors to authors id and select title and name
-#    c.execute('select title, name from articles, authors'
-#              'where articles.author = authors.id')
-#    posts = c.fetchall()
- #   db.commit()
-#    db.close()
-#    print(posts)
-#    return posts
-
-#def combine_article_log():
- #   db = psycopg2.connect(datebase = DBNAME)
- #   c = db.cursor()
-    # match article name in article table to path name in log table
-#    c.execute('select path, title from log '
-#              'where path like articles.title' )
-             # 'where articles.title = log.path')
-
 def create_views_article_path():
     """creates new table called article_path by splitting log.path from the /article/"""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     # count total of paths in descending order# #
-    c.execute('CREATE or REPLACE view article_path AS '
-              'SELECT path, SPLIT_PART(path, '/', 3) AS'
-              'article_name FROM log;')
+    c.execute("CREATE or REPLACE view article_path AS "
+              "SELECT path, split_part(path, '/', 3) AS "
+              "article_name FROM log;")
+    c.execute("select * from article_path limit 10;")
     posts = c.fetchall()
-
+    db.commit()
     db.close()
-    print(posts)
+    #print(posts)
     return posts
 
+create_views_article_path()
 
-
-def count_best_articles():
+# Most Popular Articles
+def count_top_articles():
     """Return all posts from the database"""
     db = psycopg2.connect(database = DBNAME)
     c = db.cursor()
-    # delete spam posts from log table
-    delete_spam()
+    # delete spam posts from log table - not needed since they aren't in the slug
+    #delete_spam()
     # count total of paths in descending order# #
-    c.execute('SELECT title, count(*) AS views'
-              'FROM articles, article_path'
-              'WHERE articles.slug = article_path.article_name'
-              'GROUP BY article_path.article_name, articles.title'
-              'ORDER BY views DESC LIMIT 3;'
-              #'SELECT path, count(*) AS num '
-              #'FROM log GROUP BY path '
-              #'ORDER BY num DESC LIMIT 10;')
+    c.execute("SELECT title, count(*) AS num_views "
+              "FROM articles, article_path "
+              "WHERE articles.slug = article_path.article_name "
+              "GROUP BY article_path.article_name, articles.title "
+              "ORDER BY num_views DESC LIMIT 3;")
     posts = c.fetchall()
-
     db.close()
-    print(posts)
+    print("\nMost Popular Articles:")
+    for post in posts:
+        print('\"{0}\" - {1} views'.format(str(post[0]), str(post[1])))
     return posts
 
-count_best_articles()
+count_top_articles()
+
+# Most Popular Authors
+def count_popular_authors():
+    db = psycopg2.connect(database = DBNAME)
+    c = db.cursor()
+    # sum the views of all articles written by author
+    c.execute("SELECT name, count(*) AS num "
+              "FROM authors, articles, article_path "
+              "WHERE authors.id = articles.author "
+              "AND article_path.article_name = articles.slug "
+              "GROUP BY authors.name "
+              "ORDER BY num DESC")
+    posts = c.fetchall()
+    db.close()
+    print("\nMost Popular Authors:")
+    for post in posts:
+        print("{0} - {1} views".format(str(post[0]), str(post[1])))
+    return posts
+
+count_popular_authors()
